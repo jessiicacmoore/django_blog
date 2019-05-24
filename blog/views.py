a55bff70759a8f5c4bef9cb8b563d270b62efa97
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-24 17:39:11 -0400

    Completed part 2

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from datetime import date
+from blog.models import Article
 
 def root(request):
     return HttpResponseRedirect('home')
 
 def home_page(request):
-    today = date.today()
-    context = {'todays_date': today}
+    context = {'articles': Article.objects.filter(draft=False).order_by('-published_date')}
     response = render(request, 'index.html', context)
     return HttpResponse(response)
